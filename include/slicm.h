ff3ee3b94aa5f6195a5c5cc7af0946821f651a55
Morphling89 <Morphling89@users.noreply.github.com>
2017-02-21 13:00:01 -0500

    first

---
+++ include/slicm.h
@@ -0,0 +1,8 @@
+/*
+ * File: slicm.h
+ *
+ *      This is a slicm header file that is global to the entire project.
+ *      It is located here so that everyone will find it.
+ */
+extern int compute_slicm (int a);
+
---
+++ tools/slicm/Release+Asserts/main.d
@@ -0,0 +1,5 @@
+/home/mhafiz/eecs583/slicm/tools/slicm/Release+Asserts/main.o \
+  /home/mhafiz/eecs583/slicm/tools/slicm/Release+Asserts/main.d: main.c \
+  /home/mhafiz/eecs583/slicm/include/slicm.h
+
+/home/mhafiz/eecs583/slicm/include/slicm.h:
